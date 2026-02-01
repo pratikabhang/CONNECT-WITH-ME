47817c26e1c25db4292d74ef7082415615c09cea
pratikabhang <pratikabhang2003@gmail.com>
2026-02-01 19:04:00 +0530

    github

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// Function to open a URL in a new tab
 function openUrl(url) {
     window.open(url, '_blank');
 }
-
-// Function to copy text to clipboard and show a toast message
 function copyToClipboard(event, text) {
-    event.stopPropagation(); // Prevent parent click event
-
+    event.stopPropagation();
     navigator.clipboard.writeText(text).then(() => {
         showToast("Link copied to clipboard!");
     }).catch(err => {
@@ -14,25 +10,19 @@ function copyToClipboard(event, text) {
         showToast("Failed to copy link");
     });
 }
-
-// Function to show a toast message
 function showToast(message) {
     const toast = document.getElementById("toast");
     toast.innerText = message;
     toast.classList.add("show");
-
     setTimeout(() => {
         toast.classList.remove("show");
     }, 2000);
 }
-
-// Disable zoom via keyboard and mouse
 window.addEventListener('keydown', function (e) {
     if ((e.ctrlKey || e.metaKey) && (e.key === '+' || e.key === '-' || e.key === '=')) {
         e.preventDefault();
     }
 });
-
 window.addEventListener('wheel', function (e) {
     if (e.ctrlKey) {
         e.preventDefault();
